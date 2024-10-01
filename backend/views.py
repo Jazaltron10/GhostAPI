f3afd28e9cd38bbe431230674bca6986395f5a3a
Jazaltron <jazaltron10@gmail.com>
2024-10-01 02:05:45 +0100

    Second commit

--- backend/views.py
+++ backend/views.py
@@ -2,14 +2,62 @@ from django.shortcuts import render
 from .models import Article 
 from .serializers import ArticleSerializer
 from rest_framework.parsers import JSONParser
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 
+# For api view
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+# for class based views
+from rest_framework.views import APIView
 
+
+
+class ArticleList(APIView):
+    
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer =ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class ArticleDetails(APIView):
+    def get_object(self, slug):
+        try:
+            return Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, slug):
+        article = self.get_object(slug)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    def put(self, request, slug):
+        article = self.get_object(slug)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, slug):
+        article = self.get_object(slug)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)        
+        
+        
+        
+        
+"""
 @api_view(['GET', 'POST'])
 def article_list(request):
     if request.method == 'GET':
@@ -47,10 +95,7 @@ def article_details(request, slug):
     elif request.method == "DELETE":
         article.delete() 
         return Response(status=status.HTTP_204_NO_CONTENT)    
-
-
-
-
+"""
 
 
 
@@ -301,6 +346,13 @@ def article_details(request, slug):
 
 
 """
+from django.shortcuts import render
+from .models import Article 
+from .serializers import ArticleSerializer
+from rest_framework.parsers import JSONParser
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 @csrf_exempt
 def article_list(request):
     #get all articles
