d263a47610804b61325f570a56401a0856805fef
Jazaltron <jazaltron10@gmail.com>
2024-10-02 22:41:22 +0100

    commit  - viewsets and routers

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path 
+from django.urls import include, path 
 # from .views import article_list, article_details
-from .views import ArticleList, ArticleDetails
+# from .views import ArticleList, ArticleDetails
+from .views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
 
 urlpatterns = [
-    path('articles/', ArticleList.as_view()),
-    path('articles/<slug:slug>/', ArticleDetails.as_view())
+    path('api/', include(router.urls))
+    # path('articles/', ArticleList.as_view()),
+    # path('articles/<slug:slug>/', ArticleDetails.as_view())
     
     # path('articles/', article_list),
     # path('articles/<slug:slug>/', article_details)
